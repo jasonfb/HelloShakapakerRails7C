d1987733393a7264eb4647074609e5efc162e82d
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2022-05-28 13:47:05 -0400

    adds @hotwired/turbo-rails using yarn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+// added from Shakapacker docs
+import React from 'react';
+import {createRoot } from 'react-dom/client';
+import Logo from './components/basic_elements/logo/Logo';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(<Logo name="World" />);
+});
